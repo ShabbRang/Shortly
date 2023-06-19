f40705bf31a06225414e40d593a4bb1ee49b9ae7
Shabbir <shabbrang90@gmail.com>
2023-06-19 21:18:41 +0530

    temp commit

---
+++ src/components/Center.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Center.css';
+
+export default function Center() {
+  return (
+    <div>
+      <div className="cp">
+        <h1>More Than Just Shorter Links</h1>
+      </div>
+      <div className="desc">
+        <p>Build your brand's recongnition and get detailed insights on how your links are performing</p>
+      </div>
+    </div>
+  );
+}
