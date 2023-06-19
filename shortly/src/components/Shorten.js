b970779eb2d3626ea84e88f87f967407707a1257
Shabbir <shabbrang90@gmail.com>
2023-06-19 10:48:06 +0530

    First Commit

--- shortly/src/components/Shorten.js
+++ shortly/src/components/Shorten.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Shorten.css';
 
 export default function Shorten() {
+
+    const [long_url, setlong_url] = useState('');
+
+    const changeValue = (event) => {
+        setlong_url(event.target.value)
+    }
+
+    const short = () => {
+       if(long_url != ""){
+        
+       }
+       else{
+        console.log("Somethings never change")
+       }
+    };
+
     return (
         <div>
             <div className="url">
                 <div className="long_url">
                     <form action="">
-                        <input type="text" name="long_url" id="long_url" placeholder='Add The Long' />
-                        <button type="button">Shorten It</button>
+                        <input type="text" value={long_url} onChange={changeValue} placeholder='Add The Long' />
+                        <button type="button" onClick={short}>Shorten It</button>
                     </form>
                 </div>
                 <div className="short_url">
                     <form action="">
-                        <input type="text" name="long_url" id="long_url"  />
-                        <button type="button">Copy</button>
+                        <input type="text" name="short_url" id="short_url"  />
+                        <button type="button" >Copy</button>
                     </form>
                 </div>
             </div>
